2a304703465ce1402c323c9c4ae056a4d846d6ba
NVHorbachuk <horbachuknazar@gmail.comgit config --global user.email>
2025-04-08 23:06:33 +0300

    Оновив HW1.js та index.html

--- HW1/HW1.js
+++ HW1/HW1.js
@@ -1,15 +1,1 @@
-// alert("Hello world"); // C
-// Comment
-// alert("Hello world");
-// console.log("Hello world");
-// console.log("5");
-
-// let const var
-
-// let name = "Nazarii";
-// name = "George"
-// console.log(name);
-const Name = "George";
-let name
-
-console.log(`name`);
+console.log("Horbachuk");
