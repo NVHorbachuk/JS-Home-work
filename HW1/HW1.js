01bf952721dc6800305608dfdabe31a4388807eb
NVHorbachuk <horbachuknazar@gmail.comgit config --global user.email>
2025-04-08 23:22:48 +0300

    2-3

--- HW1/HW1.js
+++ HW1/HW1.js
@@ -1,1 +1,11 @@
-console.log("Horbachuk");
+// console.log("Horbachuk");
+let primitiveTypes = {
+    stringType: "Це рядок",        // String
+    numberType: 123,               // Number
+    booleanType: true,             // Boolean
+    undefinedType: undefined,      // Undefined
+    nullType: null                 // Null
+  };
+  
+  console.log(primitiveTypes);
+  
