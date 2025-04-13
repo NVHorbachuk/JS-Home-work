9047ece3a504abd006dd42872bbff8ee37a5c329
NVHorbachuk <horbachuknazar@gmail.comgit config --global user.email>
2025-04-13 17:11:10 +0300

    Full HW1

--- HW1/HW1.js
+++ HW1/HW1.js
@@ -1,11 +1,65 @@
 // console.log("Horbachuk");
-let primitiveTypes = {
-    stringType: "Це рядок",        // String
+
+
+// Task 4
+/* let primitiveTypes = {
+    stringType: "it's line",        // String
     numberType: 123,               // Number
     booleanType: true,             // Boolean
     undefinedType: undefined,      // Undefined
     nullType: null                 // Null
   };
   
-  console.log(primitiveTypes);
+  console.log(primitiveTypes);*/
   
+// Task 5
+// let isAdult = confirm("Ви досягли повноліття?");
+// console.log("Повнолітній:", isAdult);
+
+// Task 6 Змінні користувача
+/*let firstName = "Назарій";
+let lastName = "Горбачук";
+let studyGroup = "ІТ-25";
+let birthYear = 1994;
+
+// Логічна змінна
+let isMarried = false;
+
+// Типи
+console.log("Number:", birthYear);
+console.log("Boolean:", isMarried);
+console.log("String:", firstName, lastName, studyGroup);
+
+// Змінні null та undefined
+let exampleNull = null;
+let exampleUndefined;
+
+console.log("Null type:", typeof exampleNull);
+console.log("Undefined type:", typeof exampleUndefined);*/
+
+/*const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+const daysInMonth = 30; // приблизно
+
+const secondsInHour = secondsInMinute * minutesInHour;
+const secondsInDay = secondsInHour * hoursInDay;
+const secondsInMonth = secondsInDay * daysInMonth;
+
+alert(`Секунд у годині: ${secondsInHour}`);
+alert(`Секунд у добі: ${secondsInDay}`);
+alert(`Секунд у місяці: ${secondsInMonth}`) */
+
+/*Task 8
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+const daysInMonth = 30; // приблизно
+
+const secondsInHour = secondsInMinute * minutesInHour;
+const secondsInDay = secondsInHour * hoursInDay;
+const secondsInMonth = secondsInDay * daysInMonth;
+
+alert(`Секунд у годині: ${secondsInHour}`);
+alert(`Секунд у добі: ${secondsInDay}`);
+// alert(`Секунд у місяці: ${secondsInMonth}`) */
